265908f60aeb95cc153f9a74123067bb1e85c474
muzzyay <muzzyay@gmail.com>
2019-02-05 12:46:25 -0700

    last

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".songButton").click(function () {
         var userInput = $("#songName").val().trim();
-        // console.log(userInput);
+        
         $.ajax({
             url: "https://api.spotify.com/v1/search?q=" + userInput + "&type=track&limit=5",
             headers: {
@@ -24,40 +24,7 @@ $(document).ready(function () {
                 $("#songList").append(artistName, songName, artistImage, button);
             }
             console.log(data.tracks);
-            // $("#songList").append("Artist Name: " + data.tracks.items[0].artists[0].name);
-            // var artistName = $("<h4>").text("Artist Name: " + data.tracks.items[0].artists[0].name);
-            // data.tracks.items[0].artists[0].name
-            // var button = $("<button id= btn0>").text("THIS ONE");
-            // $("#songList").append(artistName, artistImage, button);
-
-            // var artistName = $("<h4>").text("Artist Name: " + data.tracks.items[1].artists[0].name);
-            // var artistImage = $("<img>").attr("src", data.tracks.items[1].album.images[2].url);
-            // var button = $("<button id= bt1n>").text("THIS ONE");
-            // $("#songList").append(artistName, artistImage, button);
-
-            // var artistName = $("<h4>").text("Artist Name: " + data.tracks.items[2].artists[0].name);
-            // var artistImage = $("<img>").attr("src", data.tracks.items[2].album.images[2].url);
-            // var button = $("<button id=btn2>").text("THIS ONE");
-            // $("#songList").append(artistName, artistImage, button);
-
-            // var artistName = $("<h4>").text("Artist Name: " + data.tracks.items[3].artists[0].name);
-            // var artistImage = $("<img>").attr("src", data.tracks.items[3].album.images[2].url).html("<br/>");
-            // var button3 = $("<button id= btn3>").text("THIS ONE");
-            // var button3TEST = (data.tracks.items[3].id);
-            // console.log("Button 3 test", button3TEST)
-            // var button3URI = $("#btn3").attr(data.tracks.items[0]);
-            // console.log("#btn3", button3);
-            // $("#songList").append(artistName, artistImage, button3);
-
-
-            // $("#btn3").click(function () {
-            //     // event.preventDefault();
-            //     var button3URI = button3TEST;
-            //     console.log(button3URI);
-            //     // $(this).event(data.tracks.items[0].uri);
-            //     console.log(button3URI);
-            //     $("#songList").empty();
-            // })
+           
 
         });
     });
@@ -76,7 +43,7 @@ $(document).on("click", "#btn", function () {
         method: "POST",
     }).then(function (data) {
 
-        // console.log(data.tracks.items);
+        
     })
     setTimeout(refresh, 30000);
 });
