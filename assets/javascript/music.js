a18b68055f3194b11a28a24ae887810bbfd951f7
Danelle Pontzious <danellepontzious@Danelles-MacBook-Air.local>
2019-02-07 09:51:15 -0700

    merge

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
-                "Authorization": "Bearer BQAZDNDXO6BN7rly5WyXLAhK1xcfeySO-bax_Zhfpmv8mNdbkK-4Uf41CP1Tj2oLLnRBYjJHBM6qI8hkFWt8eyOTIiMgoXt4rbkMtI99iRgvj6Q9CibWjQ04Yuqoar33FwAneoFVAUJX8oELiDxqoHU4Iim9WUMdy1UxwmOwYQMmaXAsEjqKjVhDJ3ZBNxfJXRCyYoe04HisK8XBAJOlkxnY_l6Y2-CjUQIZztvoIXbn_GQ",
+                "Authorization": "Bearer BQD_caSeM5WZeMcwoFqm6wXAgFgUvYXvBBZgDVEUkkLUDijyyzc8qFFGjp_AKFvf--Dc7ogUnsQO9wjmFCQRa4I-gcL9rp0X7OwbA8v0KwqdK3dWHNQOYtQjojHghSViLGf3HSkOOdv_7qk5P9wU7hvH7jl0JQOAy5xJHpKcsj_XIxIEYuyKgphrOJBTMj6bzf0Hv0ozDHX1PSFMWY9HYOFn3oiHz-3C3fCRJntivsg5SVM",
             },
             "expires_in": 999999999999999999999999999999999999999,
         }).then(function (data) {
@@ -46,7 +46,7 @@ $(document).ready(function () {
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
-                "Authorization": "Bearer BQAZDNDXO6BN7rly5WyXLAhK1xcfeySO-bax_Zhfpmv8mNdbkK-4Uf41CP1Tj2oLLnRBYjJHBM6qI8hkFWt8eyOTIiMgoXt4rbkMtI99iRgvj6Q9CibWjQ04Yuqoar33FwAneoFVAUJX8oELiDxqoHU4Iim9WUMdy1UxwmOwYQMmaXAsEjqKjVhDJ3ZBNxfJXRCyYoe04HisK8XBAJOlkxnY_l6Y2-CjUQIZztvoIXbn_GQ",
+                "Authorization": "Bearer BQD_caSeM5WZeMcwoFqm6wXAgFgUvYXvBBZgDVEUkkLUDijyyzc8qFFGjp_AKFvf--Dc7ogUnsQO9wjmFCQRa4I-gcL9rp0X7OwbA8v0KwqdK3dWHNQOYtQjojHghSViLGf3HSkOOdv_7qk5P9wU7hvH7jl0JQOAy5xJHpKcsj_XIxIEYuyKgphrOJBTMj6bzf0Hv0ozDHX1PSFMWY9HYOFn3oiHz-3C3fCRJntivsg5SVM",
             },
             "expires_in": 999999999999999999999999999999999999999,
             method: "POST",
